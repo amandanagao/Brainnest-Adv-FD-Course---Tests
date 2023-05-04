c880886c02b88e87b58579839d7baab5c4574559
amandanagao <nagaoamanda@gmail.com>
2023-05-04 15:48:59 +0100

    Updated tests

--- Tests/analyzeArray.test.js
+++ Tests/analyzeArray.test.js
@@ -1,21 +1,25 @@
 const onlyNumbers = require('../Functions/analyzeArray');
 
-test('1. Received only numbers - create object', () => {
-    expect(onlyNumbers([1,2,3,4,5])).toStrictEqual({average: 3, min: 1, max: 5, length: 5});
+describe('Return true and create an object', () => {
+    test('1. Received only numbers', () => {
+        expect(onlyNumbers([1,2,3,4,5])).toStrictEqual({average: 3, min: 1, max: 5, length: 5});
+    });
+
+    test('2. Received only numbers', () => {
+        expect(onlyNumbers([45,27,32,81,102,21])).toStrictEqual({average: 51, min: 21, max: 102, length: 6});
+    });
+
+    test('3. Received only numbers', () => {
+        expect(onlyNumbers([2,-5,4])).toStrictEqual({average: 0, min: -5, max: 4, length: 3});
+    });
 });
 
-test('2. Received only numbers - create object', () => {
-    expect(onlyNumbers([45,27,32,81,102,21])).toStrictEqual({average: 51, min: 21, max: 102, length: 6});
-});
+describe('Return false', () => {
+    test('4. Received string', () => {
+        expect(onlyNumbers([1,2,'three',4,5])).toBeFalsy();
+    });
 
-test('3. Received only numbers - create object', () => {
-    expect(onlyNumbers([2,-5,4])).toStrictEqual({average: 0, min: -5, max: 4, length: 3});
-});
-
-test('4. Received string - return false', () => {
-    expect(onlyNumbers([1,2,'three',4,5])).toBeFalsy();
-});
-
-test('4. Received null and "" - return false', () => {
-    expect(onlyNumbers([52,"", 84, null, 98])).toBeFalsy();
+    test('4. Received null and ""', () => {
+        expect(onlyNumbers([52,"", 84, null, 98])).toBeFalsy();
+    });
 });
--- Tests/calculator.test.js
+++ Tests/calculator.test.js
@@ -1,33 +1,41 @@
 const mathOperations = require('../Functions/calculator');
 
-test('1. Sum: Adds 1 + 2 to equal 3', () => {
-  expect(mathOperations.sum(1, 2)).toBe(3);
+describe('Sum', () => {
+    test('1. Adds 1 + 2 to equal 3', () => {
+        expect(mathOperations.sum(1, 2)).toBe(3);
+    });
+
+    test('2. Adds -7 + 5 to equal -2', () => {
+        expect(mathOperations.sum(-7, 5)).toBe(-2);
+    });
 });
 
-test('2. Sum: Adds -7 + 5 to equal -2', () => {
-  expect(mathOperations.sum(-7, 5)).toBe(-2);
+describe('Subtract', () => {
+    test('1. Subtracts 2 - 1 to equal 1', () => {
+        expect(mathOperations.subtract(2, 1)).toBe(1);
+    });
+
+    test('2. Subtracts 4 - 9 to equal -5', () => {
+        expect(mathOperations.subtract(4, 9)).toBe(-5);
+    });
 });
 
-test('1. Subtract: Subtracts 2 - 1 to equal 1', () => {
-  expect(mathOperations.subtract(2, 1)).toBe(1);
+describe('Divide', () => {
+    test('1. Divides 10 / 5 to equal 2', () => {
+        expect(mathOperations.divide(10, 5)).toBe(2);
+    });
+
+    test('2. Divides 10 / 0 to return error', () => {
+        expect(mathOperations.divide(10, 0)).toBe('Error');
+    });
 });
 
-test('2. Subtract: Subtracts 4 - 9 to equal -5', () => {
-  expect(mathOperations.subtract(4, 9)).toBe(-5);
-});
+describe('Multiply', () => {
+    test('1. Multiplies 3 * 3 to equal 9', () => {
+        expect(mathOperations.multiply(3, 3)).toBe(9);
+    });
 
-test('1. Divide: Divides 10 / 5 to equal 2', () => {
-  expect(mathOperations.divide(10, 5)).toBe(2);
-});
-
-test('2. Divide: Divides 10 / 0 to return error', () => {
-  expect(mathOperations.divide(10, 0)).toBe('Error');
-});
-
-test('1. Multiply: Multiplies 3 * 3 to equal 9', () => {
-  expect(mathOperations.multiply(3, 3)).toBe(9);
-});
-
-test('2. Multiply: Multiplies 5 * 0 to equal 0', () => {
-  expect(mathOperations.multiply(5, 0)).toBe(0);
+    test('2. Multiplies 5 * 0 to equal 0', () => {
+        expect(mathOperations.multiply(5, 0)).toBe(0);
+    });
 });
